43df30c3cdf7ec8a22c34496c343ca4e089cf8c5
Alejandro Marrero <crixto@gmail.com>
2022-05-11 15:09:14 +0200

    New react project first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+import Characters from "./components/Characters";
+
+function App() {
+  return (
+      <Characters />
+  );
+}
+
+export default App;

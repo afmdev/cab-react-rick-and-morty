2efe3597b759fadf5d9a5ac9f37504380914368b
Alejandro Marrero <crixto@gmail.com>
2022-05-12 09:56:37 +0200

    More changes on the modal

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -7,36 +7,36 @@ const [data, setData] = useState(null);
 const [loading, setLoading] = useState(true);
 const [error, setError] = useState(null);
 
-useEffect(() => {
-const getData = async () => {
-  try {
-    const response = await fetch(
-      `https://rickandmortyapi.com/api/character`
-    );
-    if (!response.ok) {
-      throw new Error(
-        `This is an HTTP error: The status is ${response.status}`
+  
+  const getData = async () => {
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character`
       );
+      if (!response.ok) {
+        throw new Error(
+          `This is an HTTP error: The status is ${response.status}`
+        );
+      }
+      let actualData = await response.json();
+      let pages = actualData.info.pages
+      console.log(pages)
+      let charactersData = [...actualData.results]
+      setData(charactersData);
+      setError(null);
+    } catch (err) {
+      setError(err.message);
+      setData(null);
+    } finally {
+      setLoading(false);
     }
-    let actualData = await response.json();
-    let pages = actualData.info.pages
-    console.log(pages)
-    let charactersData = [...actualData.results]
-    setData(charactersData);
-    setError(null);
-  } catch(err) {
-    setError(err.message);
-    setData(null);
-  } finally {
-    setLoading(false);
-  }  
-}
+  }
+useEffect(() => {
 getData()
 }, [])
 
 return (
   <div className="App">
-    <h1>API Posts</h1>
     {loading && <div>A moment please...</div>}
     {error && (
       <div>{`There is a problem fetching the post data - ${error}`}</div>
@@ -44,6 +44,8 @@ return (
     <div className="container">
       {data &&
         data.map(({ id, name, image }) => (
+          <div>
+          <Modal id={id} name={name} image={image}/>
           <div className="flip-card" key={id}>
             <div className="flip-card-inner">
               <div className="flip-card-front">
@@ -52,17 +54,15 @@ return (
               <div className="flip-card-back">
                 <h3>{name}</h3>
                 {/* <a href="https://www.google.es" target="_blank" rel="noreferrer">READ MORE</a> */}
-                <Modal id={id} name={name} image={image}/>
+                
                 
               </div>
             </div>
-          </div>
+            </div>
+            </div>
         ))}
     </div>
   </div>
 );
 }
-
-
-
 

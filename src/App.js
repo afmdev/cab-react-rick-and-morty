7f3bd4ad3a191d170faa1c3e13db6864f931b5a8
Alejandro Marrero <crixto@gmail.com>
2022-05-13 10:04:24 +0200

    Last changes in modal and filter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,90 @@
-import React from "react";
 import "./index.css";
-import Characters from "./components/Characters";
+import { useState, useEffect } from "react";
+import Searchbar from "./components/Searchbar";
+import Modal from "./components/Modal";
 
-function App() {
+export default function Characters() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [filter, setFilter] = useState(data);
+  
+  
+  
+let result = []
+function handleChange(event) {
+    let myValue = event.target.value
+    result = data.filter((element) => {
+      return element.name.includes(myValue)
+    })
+  setFilter(result)
+}
+  
+  
+const getData = async () => {
+  try {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character`
+    );
+    if (!response.ok) {
+      throw new Error(
+        `This is an HTTP error: The status is ${response.status}`
+      );
+    }
+      let actualData = await response.json();
+      let charactersData = [...actualData.results]
+    setData(charactersData);
+    setFilter(charactersData)
+      setError(null);
+    } catch (err) {
+      setError(err.message);
+    setData(null);
+    
+    } finally {
+      setLoading(false);
+    }
+}
+  
+useEffect(() => {
+  getData()
+}, [])
+  
+useEffect(() => {
+  console.log("use effect running");
+}, [filter])
+
+ 
+
   return (
-      <div><Characters /></div>
-      
-  );
+  
+    <div className="App">
+      <Searchbar handleChange={handleChange} />
+     
+      <div className="Content">
+        {loading && <div>A moment please...</div>}
+
+        {error && (<div>{`There is a problem fetching the post data - ${error}`}</div>)}
+
+        {filter && filter.map(({ id, name, image, species, status }) => (
+            <div key={id}>
+              <Modal id={id} name={name} image={image} species={species} status={status} />
+              <div className="flip-card" key={id}>
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src={image} alt={name}></img>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>{name}</h3>
+                    {/* <a href="https://www.google.es" target="_blank" rel="noreferrer">READ MORE</a> */}
+                    
+                    
+                  </div>
+                </div>
+                </div>
+              </div>
+          ))}
+        </div>
+    </div>
+);
 }
 
-export default App;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,12 +5,14 @@ import "../index.css";
 function Modal(props) {
   return (
     <div className="modal-container">
-        <a href={"#open-modal-1"+props.id}>Open Modal</a>
-        <div id={"open-modal-1"+props.id} className="modal-window">
+        <a href={"#open-modal-"+props.id}>Open Modal</a>
+        <div id={"open-modal-"+props.id} className="modal-window">
         <div>
           <a href="#modal-close" title="Close" className="modal-close">close &times;</a>
           <h2>{props.name}</h2>
-          <div>{props.name}</div>
+          <div><img src={props.image} alt={props.name}></img></div>
+          <div>Species: {props.species}</div>
+          <div>Status: {props.status}</div>
         </div>
         </div>
       </div>);

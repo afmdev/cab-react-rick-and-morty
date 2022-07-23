d476567ef8a97689a08fa84e00aeeb6683baac1f
Alejandro Marrero <crixto@gmail.com>
2022-07-23 13:01:08 +0200

    Adding a footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+	return (
+		<div className="footer">
+			<p><a href="https://www.alejandrofm.com" target="_blank" rel="noopener" title="Webdesign Berlin"><img src="https://www.alejandrofm.com/wp-content/uploads/2021/11/alejandro-fm-diseno-web-berlin-wordpress-front-end-logo-black.svg" width="42" height="19" alt="Webdesign Berlin" /></a></p>
+			<p>My first Single Page Application on React.js</p>
+		</div>
+	)
+}
+
+export default Footer;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,19 +3,18 @@ import React from "react";
 import "../index.css";
 
 function Modal(props) {
-  return (
-    <div className="modal-container">
-        <a href={"#open-modal-"+props.id}>Open Modal</a>
-        <div id={"open-modal-"+props.id} className="modal-window">
-        <div>
-          <a href="#modal-close" title="Close" className="modal-close">close &times;</a>
-          <h2>{props.name}</h2>
-          <div><img src={props.image} alt={props.name}></img></div>
-          <div>Species: {props.species}</div>
-          <div>Status: {props.status}</div>
-        </div>
-        </div>
-      </div>);
+	return (
+		<div className="modal-container">
+			<div id={"open-modal-" + props.id} className="modal-window">
+				<div>
+					<a href="#modal-close" title="Close" className="modal-close">close &times;</a>
+					<h2>{props.name}</h2>
+					<div><img src={props.image} alt={props.name}></img></div>
+					<div>Species: {props.species}</div>
+					<div>Status: {props.status}</div>
+				</div>
+			</div>
+		</div>);
 }
 export default Modal;
 
